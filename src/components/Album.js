79fdf5ce6ab017ad97c06ebb5e8fcceb0e9482de
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-19 04:50:01 +0300

    FEAT: populate photos of each album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { fetchPhotos, fetchAlbums } from "../API/data";
 import { useParams } from "react-router-dom";
+import { Card, Layout, Typography, Spin, Table } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 function Album() {
   const { id } = useParams();
-  const [album, setAlbum] = useState([]);
+  const [album, setAlbum] = useState({});
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -25,11 +27,46 @@ function Album() {
         (photo) => photo.albumId === parseInt(id)
       );
       setPhotos(albumPhotos);
+      console.log(albumPhotos);
       setLoading(false);
     });
   }, [id]);
 
-  return <div>Album</div>;
+  const gridStyle = {
+    width: "25%",
+    textAlign: "center",
+  };
+
+  return (
+    <Layout className="album-layout">
+      <Typography.Title level={2} style={{ textAlign: "center" }}>
+        Album Photos
+      </Typography.Title>
+      <Content style={{ padding: "0 50px" }}>
+        <Card
+          title={album.title || "Loading..."}
+          loading={loading}
+          hoverable={false}
+        >
+          {loading ? (
+            <Spin />
+          ) : (
+            <div className="album-photos">
+              {photos.map((photo) => (
+                <Card.Grid style={gridStyle} hoverable={false}>
+                  <img
+                    src={photo.url}
+                    alt={photo.title}
+                    style={{ maxHeight: "100%", maxWidth: "100%" }}
+                  />
+                </Card.Grid>
+              ))}
+            </div>
+          )}
+        </Card>
+      </Content>
+    </Layout>
+  );
 }
 
 export default Album;
